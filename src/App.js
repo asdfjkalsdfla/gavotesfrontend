ed062b48740e8810630e54e82d9fc685b9224c41
Tom Thumb <github@asdfjkalsdfla.com>
2022-10-22 12:45:13 -0400

    update to latest deps

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import VotesRoot from "./VotesRoot";
 import PrecinctsResultToShapeMatch from "./PrecinctsResultToShapeMatch"
 import "./App.css";
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/precincts/match">
-          <PrecinctsResultToShapeMatch />
-        </Route>
-        <Route path="/">
-          <VotesRoot />
-        </Route>
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/precincts/match" element={<PrecinctsResultToShapeMatch />} />
+        <Route path="/" element={<VotesRoot />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
