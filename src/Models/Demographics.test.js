391a9b1ccc10e58e202be65a24a9b6bf1543fa2b
Tom Thumb <github@asdfjkalsdfla.com>
2022-12-16 09:00:34 -0500

    testing data in CI

--- src/Models/Demographics.test.js
+++ src/Models/Demographics.test.js
@@ -1,20 +1,43 @@
 /* eslint-disable no-underscore-dangle */
 import { describe, it, expect, beforeAll } from "vitest";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+// import fs from "fs";
+// import path from "path";
+// import { fileURLToPath } from "url";
 import Demographics from "./Demographics";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 describe.concurrent("Demographics", () => {
   let demographic;
   beforeAll(() => {
-    const fileName = path.resolve(__dirname, "../../public/static/demographics-county-2020.json");
-    const fileData = fs.readFileSync(fileName);
-    const dataRawObject = JSON.parse(fileData);
-    demographic = new Demographics(dataRawObject[0]);
+    // const fileName = path.resolve(__dirname, "../../public/static/demographics-county-2020.json");
+    // const fileData = fs.readFileSync(fileName);
+    // const dataRawObject = JSON.parse(fileData);
+    const dataRawObject = {
+      id: "COLUMBIA",
+      CTYNAME: "COLUMBIA",
+      REG20: 117248,
+      WMREG20: 36865,
+      WFMREG20: 40431,
+      WUKNREG20: 77,
+      BLMREG20: 8780,
+      BLFREG20: 10656,
+      BLUKNREG20: 31,
+      ASIANMREG2: 1671,
+      ASIANFMREG: 2114,
+      ASIANUKNRE: 10,
+      HISPMREG20: 1859,
+      HISPFMREG2: 2223,
+      HSPUKNREG2: 4,
+      OTHERMREG2: 1081,
+      OTHERFMREG: 1423,
+      OTHERUKNRE: 3,
+      UKNMALEREG: 4859,
+      UKNFMREG20: 4793,
+      UKNOWNREG2: 60,
+    };
+    demographic = new Demographics(dataRawObject);
   });
   it("validate demographic percentages", async () => {
     // check when it's way past the date but we have a higher value in past
