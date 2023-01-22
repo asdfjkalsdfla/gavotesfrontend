780b6970afe90c6cba05b278eb85cb3ce6d0cf3c
Tom Thumb <github@asdfjkalsdfla.com>
2023-01-22 11:07:56 -0500

    get rid of service worker

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,3 @@ import App from "./App.jsx";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
