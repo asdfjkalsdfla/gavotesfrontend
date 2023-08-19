e6e753e6a70647bae4aa5c86d4f3087e4f42b4f6
Tom Thumb <74577069+asdfjkalsdfla@users.noreply.github.com>
2023-08-19 18:24:37 -0400

    Add build analysis

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig(
   {
-    plugins: [react(), basicSsl()],
+    plugins: [
+      react(),
+      basicSsl(),
+      visualizer({
+        template: "treemap", // or sunburst
+        open: true,
+        gzipSize: true,
+        brotliSize: true,
+        filename: "buildAnalysis.html", // will be saved in project's root
+      }),
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
