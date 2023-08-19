f5879b5af2e258e436fbae57af4639e512af9ac6
Tom Thumb <74577069+asdfjkalsdfla@users.noreply.github.com>
2023-08-19 17:56:23 -0400

    get to TS config file

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+export default defineConfig(
+  {
+    plugins: [react(), basicSsl()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      open: "/index.html",
+      port: 3000,
+    },
+    build: {
+      outDir: "build",
+    },
+    test: {
+      base: "https://georgiavotesvisual.com/",
+      globals: true,
+      exclude: ["**/node_modules/**", "**/dist/**"],
+      setupFiles: ["./src/setupTests.js"],
+      testTimeout: 20000,
+      alias: [{ find: /^@deck.gl\/layers$/, replacement: "@deck.gl/layers/dist/esm" }],
+    }
+  });
+
