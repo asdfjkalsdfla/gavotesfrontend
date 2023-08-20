404c1793b2b5d04b75c855340b7625129629b447
Tom Thumb <74577069+asdfjkalsdfla@users.noreply.github.com>
2023-08-19 22:59:44 -0400

    lazy load precinct stuff

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import VotesRoot from "./VotesRoot.jsx";
 
-// import "antd/dist/reset.css";
 import "./App.css";
 
-// FIX - const PrecinctsResultToShapeMatch = React.lazy(() => import("./PrecinctsResultToShapeMatch.jsx"));
+const PrecinctsResultToShapeMatch = React.lazy(() => import("./PrecinctsResultToShapeMatch.jsx"));
 // import PrecinctsResultToShapeMatch from "./PrecinctsResultToShapeMatch";
 
 const router = createBrowserRouter([
   {
     path: "/precincts/match",
-    // element: <PrecinctsResultToShapeMatch />,
+    element: <PrecinctsResultToShapeMatch />,
   },
   {
     path: "/",
