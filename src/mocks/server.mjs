2a7fb0c05b94226584b08dde56b2c5631bf1d091
Tom Thumb <74577069+asdfjkalsdfla@users.noreply.github.com>
2024-10-13 14:23:42 -0400

    Add mocking of data

--- src/mocks/server.mjs
+++ src/mocks/server.mjs
@@ -1,6 +1,6 @@
 import { setupServer } from "msw/node";
 
-import { handlers } from "./handlers";
+import { handlers } from "./handlers.mjs";
 
 // This configures a request mocking server with the given request handlers.
 
