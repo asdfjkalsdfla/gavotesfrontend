fe63ddd26474bb11707fd8a1415e4503ba9fdaf3
Tom Thumb <74577069+asdfjkalsdfla@users.noreply.github.com>
2024-11-17 13:57:47 -0500

    updates test of table to use new context

--- src/table/VotesTable.test.jsx
+++ src/table/VotesTable.test.jsx
@@ -5,37 +5,42 @@ import { it, describe } from "vitest";
 import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 // import "@testing-library/jest-dom";
 import VotesTable from "./VotesTable.jsx";
+import { ElectionSelectionContext } from "../ElectionSelectionContext.tsx";
 import { ElectionDataProvider } from "../ElectionDataProvider.jsx";
 
 describe("Votes Table", () => {
   it("renders without crashing", async () => {
     render(
-      <ElectionDataProvider
-        isCountyLevel={true}
-        countyFilter={null}
-        absenteeElectionBaseID="2022_general"
-        absenteeElectionCurrentID="2022_general"
-        resultsElectionRaceCurrentID="2022_general||US Senate"
-        resultsElectionRacePerviousID="2020_general||President of the United States"
+      <ElectionSelectionContext.Provider
+        value={{
+          absenteeElectionCurrentID: "2022_general",
+          absenteeElectionBaseID: "2022_general",
+          resultsElectionRaceCurrentID: "2022_general||US Senate",
+          resultsElectionRacePerviousID: "2020_general||President of the United States",
+        }}
       >
-        <VotesTable />
-      </ElectionDataProvider>,
+        <ElectionDataProvider isCountyLevel={true} countyFilter={null}>
+          <VotesTable />
+        </ElectionDataProvider>
+      </ElectionSelectionContext.Provider>,
     );
     await waitFor(() => screen.getByText("State of Georgia"));
   });
 
   it("Show columns displayed", async () => {
     render(
-      <ElectionDataProvider
-        isCountyLevel={true}
-        countyFilter={null}
-        absenteeElectionBaseID="2022_general"
-        absenteeElectionCurrentID="2022_general"
-        resultsElectionRaceCurrentID="2022_general||US Senate"
-        resultsElectionRacePerviousID="2020_general||President of the United States"
+      <ElectionSelectionContext.Provider
+        value={{
+          absenteeElectionCurrentID: "2022_general",
+          absenteeElectionBaseID: "2022_general",
+          resultsElectionRaceCurrentID: "2022_general||US Senate",
+          resultsElectionRacePerviousID: "2020_general||President of the United States",
+        }}
       >
-        <VotesTable />
-      </ElectionDataProvider>,
+        <ElectionDataProvider isCountyLevel={true} countyFilter={null}>
+          <VotesTable />
+        </ElectionDataProvider>
+      </ElectionSelectionContext.Provider>,
     );
     await waitFor(() => screen.getByText("State of Georgia"));
     fireEvent.click(screen.getByTestId("dataElementSettings"));
