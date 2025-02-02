4d5ae95913b5e1a38f60137632a06da8dbb1cc75
Tom Thumb <74577069+asdfjkalsdfla@users.noreply.github.com>
2025-02-01 19:14:23 -0500

    move to use tailwind directly

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import path from "path";
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import { visualizer } from "rollup-plugin-visualizer";
 
@@ -9,6 +10,7 @@ export default defineConfig(
   {
     plugins: [
       react(),
+      tailwindcss(),
       basicSsl(),
       visualizer({
         template: "treemap", // or sunburst
